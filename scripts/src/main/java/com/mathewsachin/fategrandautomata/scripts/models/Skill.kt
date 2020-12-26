62c1037f7b89e29c78f2f73a192d1052ec432cd0
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-12-26 21:32:19 +0530

    Preliminary wide-screen adjustments for Battle screen

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/Skill.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/Skill.kt
@@ -1,55 +1,31 @@
 package com.mathewsachin.fategrandautomata.scripts.models
 
-import com.mathewsachin.fategrandautomata.scripts.enums.GameServerEnum
-import com.mathewsachin.fategrandautomata.scripts.modules.Phone
-import com.mathewsachin.libautomata.Location
-import com.mathewsachin.libautomata.Region
+sealed class Skill(val autoSkillCode: Char) {
+    sealed class Servant(autoSkillCode: Char) : Skill(autoSkillCode) {
+        object A1 : Servant('a')
+        object A2 : Servant('b')
+        object A3 : Servant('c')
 
-sealed class Skill(
-    val clickLocation: Location,
-    val autoSkillCode: Char
-) {
-    class Servant private constructor(
-        clickLocation: Location,
-        autoSkillCode: Char
-    ) : Skill(clickLocation, autoSkillCode) {
+        object B1 : Servant('d')
+        object B2 : Servant('e')
+        object B3 : Servant('f')
+
+        object C1 : Servant('g')
+        object C2 : Servant('h')
+        object C3 : Servant('i')
+
         companion object {
-            val jpfs get() = when {
-                Phone.s.contains("Pixel 4 XL") -> 0
-                Phone.s.contains("SM-G") -> 100
-                else -> 100
-            }
-            val list = listOf(
-                Servant(Location(jpfs+0, 1100), 'a'),
-                Servant(Location(jpfs+180, 1100), 'b'),
-                Servant(Location(jpfs+360, 1100), 'c'),
-
-                Servant(Location(jpfs+640, 1100), 'd'),
-                Servant(Location(jpfs+820, 1100), 'e'),
-                Servant(Location(jpfs+990, 1100), 'f'),
-
-                Servant(Location(jpfs+1270, 1100), 'g'),
-                Servant(Location(jpfs+1450, 1100), 'h'),
-                Servant(Location(jpfs+1630, 1100), 'i')
-            )
+            val list = listOf(A1, A2, A3, B1, B2, B3, C1, C2, C3)
         }
     }
 
-    class Master private constructor(
-        clickLocation: Location,
-        autoSkillCode: Char
-    ) : Skill(clickLocation, autoSkillCode) {
+    sealed class Master(autoSkillCode: Char) : Skill(autoSkillCode) {
+        object A : Master('j')
+        object B : Master('k')
+        object C : Master('l')
+
         companion object {
-            val jpfs get() = when {
-                Phone.s.contains("Pixel 4 XL") -> 0
-                Phone.s.contains("SM-G") -> -100
-                else -> -100
-            }
-            val list = listOf(
-                Master(Location(jpfs+1950, 640), 'j'),
-                Master(Location(jpfs+2120, 640), 'k'),
-                Master(Location(jpfs+2300, 640), 'l')
-            )
+            val list = listOf(A, B, C)
         }
     }
 }
