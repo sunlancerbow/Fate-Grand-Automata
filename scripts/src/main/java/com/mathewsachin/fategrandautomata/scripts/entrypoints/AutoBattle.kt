49876884d6b0b9a69d049edc183692e1eb7a32a9
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-12-26 15:57:36 +0530

    Party selection wide-screen

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoBattle.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoBattle.kt
@@ -525,7 +525,7 @@ open class AutoBattle @Inject constructor(
     private fun startQuest() {
         selectParty()
 
-        Game.menuStartQuestClick.click()
+        game.menuStartQuestClick.click()
 
         2.seconds.wait()
 
